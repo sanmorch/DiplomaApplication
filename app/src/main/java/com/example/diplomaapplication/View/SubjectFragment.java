7714a1747a9b2b9326a6121988d452ddf85bbb97
Alexandra Morozova <samorozo@mail.ru>
2023-05-19 01:20:02 +0300

    add changing data in subject page (костыль через данные из realtime database)

--- app/src/main/java/com/example/diplomaapplication/View/SubjectFragment.java
+++ app/src/main/java/com/example/diplomaapplication/View/SubjectFragment.java
@@ -2,65 +2,65 @@ package com.example.diplomaapplication.View;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.diplomaapplication.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SubjectFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SubjectFragment extends Fragment {
+    // textViews for name and description of subject
+    private TextView tvSubjectName, tvSubjectDescription;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // for navigation
+    NavController navController;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // arguments from previous fragment
+    private String arg_key, arg_name, arg_description;
 
-    public SubjectFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SubjectFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SubjectFragment newInstance(String param1, String param2) {
-        SubjectFragment fragment = new SubjectFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // TEST for getting data from DB by position
+    DatabaseReference reference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_subject, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        tvSubjectName.setText(arg_name);
+        tvSubjectDescription.setText(arg_description);
+
+    }
+
+    private void init(View view) {
+        // init for fragment's views
+        tvSubjectName = view.findViewById(R.id.subject_name);
+        tvSubjectDescription = view.findViewById(R.id.subject_desc);
+
+        // arguments
+        arg_key = SubjectFragmentArgs.fromBundle(getArguments()).getSubjectId();
+        arg_name = SubjectFragmentArgs.fromBundle(getArguments()).getSubjectName();
+        arg_description = SubjectFragmentArgs.fromBundle(getArguments()).getSubjectDesc();
+
+
+        // init for navigation
+        navController = Navigation.findNavController(view);
+
+        // TEST init for DB
+        reference = FirebaseDatabase.getInstance().getReference("Subjects");
     }
 }
